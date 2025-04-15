2f1d693d681c2173829b4bb2d588974707864b1f
Pradyumna Sangamkar <158041321+pradyumnasangamkar@users.noreply.github.com>
2025-04-15 20:27:06 +0530

    added operator folder

---
+++ Operators/arithmatic.java
@@ -0,0 +1,12 @@
+public class ArithmeticExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("a + b = " + (a + b)); // Addition
+        System.out.println("a - b = " + (a - b)); // Subtraction
+        System.out.println("a * b = " + (a * b)); // Multiplication
+        System.out.println("a / b = " + (a / b)); // Division
+        System.out.println("a % b = " + (a % b)); // Modulus (remainder)
+    }
+}
